5234d303cbc7594d44f8f90ac867f9db3cc0259b
Francis <francisjkavanagh@gmail.com>
2025-11-06 11:00:10 +0000

    Commented repo and tasks
    Copied in repo link and added tasks to follow along easier while working

--- src/main/java/com/mycompany/gradesort/GradeSort.java
+++ src/main/java/com/mycompany/gradesort/GradeSort.java
@@ -1,6 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+// Repo Link: https://github.com/cptbuff/GradeSort
 
 package com.mycompany.gradesort;
 
@@ -14,3 +12,15 @@ public class GradeSort {
         
     }
 }
+
+//-Create an array to store 10 integer grades (values between 0 and 100).
+//-Allow the user to input the grades using a loop.
+//-Calculate and display:
+//    -The average grade.
+//    -The highest grade.
+//    -The lowest grade.
+//    -The number of students who passed (grade ≥ 50).
+//    -The number of students who failed (grade < 50).
+//-Sort the array in ascending order and display the sorted grades.
+//💡-Display the grades in descending order.
+//-Count how many students scored above the average.
