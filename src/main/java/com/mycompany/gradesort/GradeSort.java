e2d107abb7707fb3f24ff65d926809c0a2f313dc
Francis <francisjkavanagh@gmail.com>
2025-11-06 10:42:40 +0000

    Initial Commit
    Tasks:
    -Create an array to store 10 integer grades (values between 0 and 100).
    -Allow the user to input the grades using a loop.
    -Calculate and display:
    --The average grade.
    --The highest grade.
    --The lowest grade.
    --The number of students who passed (grade ≥ 50).
    --The number of students who failed (grade < 50).
    -Sort the array in ascending order and display the sorted grades.
    -Display the grades in descending order.
    -Count how many students scored above the average.

---
+++ src/main/java/com/mycompany/gradesort/GradeSort.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradesort;
+
+/**
+ *
+ * @author francis
+ */
+public class GradeSort {
+
+    public static void main(String[] args) {
+        
+    }
+}
